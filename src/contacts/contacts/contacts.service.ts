4b7a23b8c365305d2c25ae15b4e0ad9547f9b144
Deary <chickenflakes9@gmail.com>
2019-08-19 17:52:46 +0800

    first commit

---
+++ src/contacts/contacts/contacts.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Contact } from './contact.entity';
+
+@Injectable()
+export class ContactsService {
+    constructor(
+        @InjectRepository(Contact)
+        private contactRepository: Repository<Contact>,
+    ) { }
+}
